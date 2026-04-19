b9d2a225049cae58db6e2110f7a74f2e3d957daf
Michael Atsuya <michael.atsuya@icloud.com>
2026-04-19 01:35:12 -0400

    comment out simple and ngos imports because files were deleted; update course_routes to use database instead of mock data

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -4,8 +4,8 @@ import os
 import logging
 
 from backend.db_connection import init_app as init_db
-from backend.simple.simple_routes import simple_routes
-from backend.ngos.ngo_routes import ngos
+#from backend.simple.simple_routes import simple_routes
+#from backend.ngos.ngo_routes import ngos
 from backend.courses.course_routes import courses
 from backend.students.student_routes import students
 from backend.professors.professor_routes import professors
@@ -34,8 +34,8 @@ def create_app():
     # Register the routes from each Blueprint with the app object
     # and give a url prefix to each.
     app.logger.info("create_app(): registering blueprints")
-    app.register_blueprint(simple_routes)
-    app.register_blueprint(ngos, url_prefix="/ngo")
+    #app.register_blueprint(simple_routes)
+    #app.register_blueprint(ngos, url_prefix="/ngo")
     app.register_blueprint(courses)
     app.register_blueprint(students, url_prefix='/students')
     app.register_blueprint(professors, url_prefix='/professors')
